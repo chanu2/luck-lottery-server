9c7ef6f05f9c977b54ff4ea58edce61357c5a0a9
chanwoo <mdsoo55828@gmail.com>
2024-01-11 23:58:02 +0900

    Initial commit

---
+++ src/test/java/uttugseuja/lucklotteryserver/LucklotteryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package uttugseuja.lucklotteryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucklotteryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
