530b3e16fa087e30c0fad45baa1d5022b7c81b31
chanwoo <mdsoo55828@gmail.com>
2024-01-17 22:36:16 +0900

    refactor : 리뷰 반영

--- src/main/java/uttugseuja/lucklotteryserver/global/error/ErrorResponse.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/error/ErrorResponse.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 @Getter
 public class ErrorResponse {
+
     private final boolean success = false;
     private final int status;
     private final String reason;
     private final LocalDateTime timeStamp;
     private final String path;
+
     public ErrorResponse(int status, String reason, String path) {
         this.status = status;
         this.reason = reason;
