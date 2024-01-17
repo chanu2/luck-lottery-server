c17179a4a3835ea094b4f92953803652e372ea91
chanwoo <mdsoo55828@gmail.com>
2024-01-17 21:30:03 +0900

    feat : global 예외처리 핸들러 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/error/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package uttugseuja.lucklotteryserver.global.error.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
