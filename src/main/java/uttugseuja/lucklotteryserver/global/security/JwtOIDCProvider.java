821cbd30d9f471c36c007e188035fdb888ce2a86
chanwoo <mdsoo55828@gmail.com>
2024-01-19 00:43:27 +0900

    feat : id_token에서 header와 claims 부분을 jwt로 파싱하는 기능 구현

--- src/main/java/uttugseuja/lucklotteryserver/global/security/JwtOIDCProvider.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/security/JwtOIDCProvider.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uttugseuja.lucklotteryserver.global.exception.ExpiredTokenException;
 import uttugseuja.lucklotteryserver.global.exception.InvalidTokenException;
 
 @RequiredArgsConstructor
@@ -18,5 +19,24 @@ public class JwtOIDCProvider {
         if (splitToken.length != 3) throw InvalidTokenException.EXCEPTION;
         return splitToken[0] + "." + splitToken[1] + ".";
     }
+
+    private Jwt<Header, Claims> getRemovedSignatureParsedJwt(String token, String iss, String aud) {
+        try {
+            Jwt<Header, Claims> parsedJwt = Jwts.parserBuilder()
+                    .requireAudience(aud)
+                    .requireIssuer(iss)
+                    .build()
+                    .parseClaimsJwt(removeSignatureFromToken(token));
+            return parsedJwt;
+
+        } catch (ExpiredJwtException e) {
+            throw ExpiredTokenException.EXCEPTION;
+        } catch (Exception e) {
+            log.error(e.toString());
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
+
+
 
 }
