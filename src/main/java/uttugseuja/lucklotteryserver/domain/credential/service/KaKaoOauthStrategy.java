b7a0ea3c8a20fd5e2e4bb1815a8c87f72628da8e
chanwoo <mdsoo55828@gmail.com>
2024-01-19 22:54:51 +0900

    feat : Kakao ID_TOKEN 페이로드에서 유저 정보 가져오기 기능 구현

--- src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/KaKaoOauthStrategy.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/KaKaoOauthStrategy.java
@@ -3,8 +3,8 @@ package uttugseuja.lucklotteryserver.domain.credential.service;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import uttugseuja.lucklotteryserver.global.api.client.GoogleAuthClient;
 import uttugseuja.lucklotteryserver.global.api.client.KakaoOauthClient;
+import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
 import uttugseuja.lucklotteryserver.global.property.OauthProperties;
 
 import java.security.NoSuchAlgorithmException;
@@ -18,10 +18,13 @@ public class KaKaoOauthStrategy implements OauthStrategy {
 
     private final OauthProperties oauthProperties;
     private final KakaoOauthClient kakaoOauthClient;
+    private final OauthOIDCProvider oauthOIDCProvider;
     private static final String ISSUER = "https://kauth.kakao.com";
 
+
     @Override
-    public OIDCDecodePayload getOIDCDecodePayload(String token) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return null;
+    public OIDCDecodePayload getOIDCDecodePayload(String token){
+        OIDCKeysResponse oidcKakaoKeysResponse = kakaoOauthClient.getKakaoOIDCOpenKeys();
+        return oauthOIDCProvider.getPayloadFromIdToken(token,ISSUER,oauthProperties.getKakaoAppId(),oidcKakaoKeysResponse);
     }
 }
