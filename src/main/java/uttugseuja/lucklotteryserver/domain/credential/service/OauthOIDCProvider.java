9b135e9361218bbd50c549b63a455d8ae9f41aad
chanwoo <mdsoo55828@gmail.com>
2024-01-19 22:47:44 +0900

    feat : OIDC ID_TOkEN에서 Payload body 가져오기 기능 구헌

--- src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/OauthOIDCProvider.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/OauthOIDCProvider.java
@@ -3,6 +3,8 @@ package uttugseuja.lucklotteryserver.domain.credential.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeyDto;
+import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
 import uttugseuja.lucklotteryserver.global.security.JwtOIDCProvider;
 
 @Component
@@ -15,6 +17,21 @@ public class OauthOIDCProvider {
     private String getKidFromParsedJwtIdToken(String token, String iss, String aud) {
         log.info(iss, aud);
         return jwtOIDCProvider.getKidFromParsedJwtHeader(token, iss, aud);
+    }
+
+    public OIDCDecodePayload getPayloadFromIdToken(
+            String token, String iss, String aud, OIDCKeysResponse oidcPublicKeysResponse) {
+        String kid = getKidFromParsedJwtIdToken(token, iss, aud);
+
+        OIDCKeyDto oidcKeyDto =
+                oidcPublicKeysResponse.getKeys().stream()
+                        .filter(o -> o.getKid().equals(kid))
+                        .findFirst()
+                        .orElseThrow();
+
+        return (OIDCDecodePayload)
+                jwtOIDCProvider.getOIDCTokenBody(
+                        token, oidcKeyDto.getN(), oidcKeyDto.getE());
     }
 
 
