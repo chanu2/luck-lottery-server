f623e8153995bdf8b4664fd27ec00837060ce2cb
chanwoo <mdsoo55828@gmail.com>
2024-01-19 23:19:40 +0900

    feat : GOOGLE ID_TOKEN 페이로드에서 유저 정보 가져오기 기능 구현

--- src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/GoogleOauthStrategy.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/GoogleOauthStrategy.java
@@ -3,10 +3,9 @@ package uttugseuja.lucklotteryserver.domain.credential.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import uttugseuja.lucklotteryserver.global.api.client.GoogleAuthClient;
+import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
 import uttugseuja.lucklotteryserver.global.property.OauthProperties;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 
 @AllArgsConstructor
 @Component("GOOGLE")
@@ -14,10 +13,13 @@ public class GoogleOauthStrategy implements OauthStrategy{
 
     private final OauthProperties oauthProperties;
     private final GoogleAuthClient googleAuthClient;
+    private final OauthOIDCProvider oauthOIDCProvider;
     private static final String ISSUER = "https://accounts.google.com";
+
     @Override
-    public OIDCDecodePayload getOIDCDecodePayload(String token) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return null;
+    public OIDCDecodePayload getOIDCDecodePayload(String token){
+        OIDCKeysResponse oidcKakaoKeysResponse = googleAuthClient.getGoogleOIDCOpenKeys();
+        return oauthOIDCProvider.getPayloadFromIdToken(token,ISSUER,oauthProperties.getKakaoAppId(),oidcKakaoKeysResponse);
     }
 
 
