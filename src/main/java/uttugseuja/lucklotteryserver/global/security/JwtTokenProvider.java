bcfc706b3a55b5a799944663ca236dc8344d8964
chanwoo <mdsoo55828@gmail.com>
2024-01-20 23:39:58 +0900

    feat : jwt RefreshToken 생성 기능 구현

--- src/main/java/uttugseuja/lucklotteryserver/global/security/JwtTokenProvider.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/security/JwtTokenProvider.java
@@ -18,6 +18,7 @@ public class JwtTokenProvider {
 
     private final JwtProperties jwtProperties;
     private final String ACCESS_TOKEN = "access_token";
+    private final String REFRESH_TOKEN = "refresh_token";
     private final String ROLE = "role";
     private final String TYPE = "type";
     private final String ISSUER = "lucklottery";
@@ -26,8 +27,14 @@ public class JwtTokenProvider {
         final Date issuedAt = new Date();
         final Date accessTokenExpiresIn =
                 new Date(issuedAt.getTime() + jwtProperties.getAccessExp() * 1000);
-
         return createAccessToken(id, issuedAt, accessTokenExpiresIn, accountRole);
+    }
+
+    public String generateRefreshToken(Long id) {
+        final Date issuedAt = new Date();
+        final Date refreshTokenExpiresIn =
+                new Date(issuedAt.getTime() + jwtProperties.getRefreshExp() * 1000);
+        return createRefreshToken(id, issuedAt, refreshTokenExpiresIn);
     }
 
     private String createAccessToken(
@@ -44,6 +51,17 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    private String createRefreshToken(Long id, Date issuedAt, Date accessTokenExpiresIn) {
+        final Key encodedKey = getSecretKey();
+        return Jwts.builder()
+                .setIssuer(ISSUER)
+                .setIssuedAt(issuedAt)
+                .setSubject(id.toString())
+                .claim(TYPE, REFRESH_TOKEN)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(encodedKey)
+                .compact();
+    }
 
     private Key getSecretKey() {
         return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
