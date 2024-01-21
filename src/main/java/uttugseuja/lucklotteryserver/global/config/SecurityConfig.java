c36aa092432658f165d40dc1e45484348bec920e
chanwoo <mdsoo55828@gmail.com>
2024-01-21 16:22:00 +0900

    feat : SecurityConfig 설정 추가

--- src/main/java/uttugseuja/lucklotteryserver/global/config/SecurityConfig.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/config/SecurityConfig.java
@@ -6,7 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uttugseuja.lucklotteryserver.global.security.JwtTokenFilter;
+import uttugseuja.lucklotteryserver.global.security.JwtTokenProvider;
+import uttugseuja.lucklotteryserver.global.security.filter.CustomAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -14,8 +20,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(
+                                "/**"
+                        ).permitAll()
+                )
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated());
 
         return http.build();
     }
