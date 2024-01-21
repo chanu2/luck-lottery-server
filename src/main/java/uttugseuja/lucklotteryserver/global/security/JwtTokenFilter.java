fc9b608addeb242c54a7a540c3a0319e9d65e6d9
chanwoo <mdsoo55828@gmail.com>
2024-01-21 14:03:17 +0900

    feat : JwtTokenFilter 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/security/JwtTokenFilter.java
@@ -0,0 +1,22 @@
+package uttugseuja.lucklotteryserver.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+
+    }
+}
