b9392932fb7fb84a0139b0c4630e3e3012b87856
ihun-il <hunil9978@gmail.com>
2024-01-22 14:02:18 +0900

    feat : LotteryBaseInfoVo 생성

--- src/main/java/uttugseuja/lucklotteryserver/domain/lottery/domain/Lottery.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/lottery/domain/Lottery.java
@@ -1,8 +1,10 @@
 package uttugseuja.lucklotteryserver.domain.lottery.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import uttugseuja.lucklotteryserver.domain.lottery.domain.vo.LotteryBaseInfoVo;
 import uttugseuja.lucklotteryserver.domain.user.domain.User;
 import uttugseuja.lucklotteryserver.global.common.Rank;
 
@@ -37,4 +39,37 @@ public class Lottery {
     private Integer fourthNum;
     private Integer fifthNum;
     private Integer sixthNum;
+
+    @Builder
+    public Lottery(Integer round,
+                   LocalDate winningDate,
+                   Integer firstNum,
+                   Integer secondNum,
+                   Integer thirdNum,
+                   Integer fourthNum,
+                   Integer fifthNum,
+                   Integer sixthNum) {
+        this.round = round;
+        this.winningDate = winningDate;
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+        this.thirdNum = thirdNum;
+        this.fourthNum = fourthNum;
+        this.fifthNum = fifthNum;
+        this.sixthNum = sixthNum;
+    }
+
+    public LotteryBaseInfoVo getLotteryBaseInfoVo() {
+        return LotteryBaseInfoVo.builder()
+                .lotteryId(id)
+                .round(round)
+                .winningDate(winningDate)
+                .firstNum(firstNum)
+                .secondNum(secondNum)
+                .thirdNum(thirdNum)
+                .fourthNum(fourthNum)
+                .fifthNum(fifthNum)
+                .sixthNum(sixthNum)
+                .build();
+    }
 }
