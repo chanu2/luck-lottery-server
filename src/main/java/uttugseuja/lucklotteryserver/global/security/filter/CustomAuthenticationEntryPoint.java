f4a6bcfe55dbbefd4fd1ebebe0c86b14e8308c41
chanwoo <mdsoo55828@gmail.com>
2024-01-24 17:26:54 +0900

    feat : 인증 오류 메시지 출력하는 응답 설정 기능

--- src/main/java/uttugseuja/lucklotteryserver/global/security/filter/CustomAuthenticationEntryPoint.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/security/filter/CustomAuthenticationEntryPoint.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import uttugseuja.lucklotteryserver.global.error.exception.ErrorCode;
 
 import java.io.IOException;
 
@@ -21,6 +22,13 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
+    }
+
+    private void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println("{ \"message\" : \"" + errorCode.getStatus()
+                + "\", \"status\" : \"" + errorCode.getReason()+ "\"}");
     }
 
 }
