08c97986e3fa40d769cd340bfcaf72377a5ddb77
chanwoo <mdsoo55828@gmail.com>
2024-01-24 22:57:35 +0900

    feat : 필터에서 예외를 처리할때 에러 응답을 json형태로 생성하는 기능

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/error/exception/ExceptionFilter.java
@@ -0,0 +1,40 @@
+package uttugseuja.lucklotteryserver.global.error.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+import uttugseuja.lucklotteryserver.global.error.ErrorResponse;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+
+
+    private void writeErrorResponse(HttpServletResponse response, ErrorCode errorCode, String path)
+            throws IOException {
+        ErrorResponse errorResponse =
+                new ErrorResponse(
+                        errorCode.getStatus(), errorCode.getReason(), path);
+
+        response.setStatus(errorCode.getStatus());
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+
+
+}
