71c9b1537c857a8057bb685f6a8f911646ad4bd3
chanwoo <mdsoo55828@gmail.com>
2024-01-24 16:46:24 +0900

    feat : 카카오 및 구글 캐쉬 적용

--- src/main/java/uttugseuja/lucklotteryserver/global/api/client/GoogleAuthClient.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/api/client/GoogleAuthClient.java
@@ -1,5 +1,6 @@
 package uttugseuja.lucklotteryserver.global.api.client;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
@@ -7,6 +8,7 @@ import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
 @FeignClient(name = "GoogleAuthClient", url = "https://www.googleapis.com/oauth2")
 public interface GoogleAuthClient {
 
+    @Cacheable(cacheNames = "GoogleOICD", cacheManager = "oidcKeyCacheManager")
     @GetMapping("/v3/certs")
     OIDCKeysResponse getGoogleOIDCOpenKeys();
 }
--- src/main/java/uttugseuja/lucklotteryserver/global/api/client/KakaoOauthClient.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/api/client/KakaoOauthClient.java
@@ -1,6 +1,7 @@
 package uttugseuja.lucklotteryserver.global.api.client;
 
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
@@ -8,6 +9,7 @@ import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
 @FeignClient(name = "KakaoAuthClient", url = "https://kauth.kakao.com")
 public interface KakaoOauthClient {
 
+    @Cacheable(cacheNames = "KakaoOICD", cacheManager = "oidcKeyCacheManager")
     @GetMapping("/.well-known/jwks.json")
     OIDCKeysResponse getKakaoOIDCOpenKeys();
 }
