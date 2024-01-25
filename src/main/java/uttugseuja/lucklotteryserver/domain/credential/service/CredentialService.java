7c79d8f103f8e46786778af2af7e49a210fe3e3e
chanwoo <mdsoo55828@gmail.com>
2024-01-25 13:34:33 +0900

    feat : 토큰 재발급 서비스 기능 구현

--- src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/CredentialService.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/CredentialService.java
@@ -6,13 +6,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uttugseuja.lucklotteryserver.domain.credential.domain.RefreshTokenRedisEntity;
 import uttugseuja.lucklotteryserver.domain.credential.domain.repository.RefreshTokenRedisEntityRepository;
+import uttugseuja.lucklotteryserver.domain.credential.exception.RefreshTokenExpiredException;
 import uttugseuja.lucklotteryserver.domain.credential.presentation.dto.request.RegisterRequest;
 import uttugseuja.lucklotteryserver.domain.credential.presentation.dto.response.AuthTokensResponse;
 import uttugseuja.lucklotteryserver.domain.credential.presentation.dto.response.CheckRegisteredResponse;
 import uttugseuja.lucklotteryserver.domain.user.domain.User;
 import uttugseuja.lucklotteryserver.domain.user.domain.repository.UserRepository;
+import uttugseuja.lucklotteryserver.global.exception.InvalidTokenException;
 import uttugseuja.lucklotteryserver.global.exception.UserNotFoundException;
 import uttugseuja.lucklotteryserver.global.security.JwtTokenProvider;
+import uttugseuja.lucklotteryserver.global.utils.user.UserUtils;
 
 
 import java.security.NoSuchAlgorithmException;
@@ -30,6 +33,7 @@ public class CredentialService {
     private final OauthFactory oauthFactory;
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenRedisEntityRepository refreshTokenRedisEntityRepository;
+    private final UserUtils userUtils;
 
     public CheckRegisteredResponse getUserAvailableRegister(String token, OauthProvider oauthProvider) throws NoSuchAlgorithmException, InvalidKeySpecException {
         OauthStrategy oauthstrategy = oauthFactory.getOauthstrategy(oauthProvider);
@@ -86,6 +90,31 @@ public class CredentialService {
         String accessToken =
                 jwtTokenProvider.generateAccessToken(user.getId(), user.getAccountRole());
         String refreshToken = generateRefreshToken(user.getId());
+
+        return AuthTokensResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public AuthTokensResponse tokenRefresh(String requestRefreshToken) {
+
+        Optional<RefreshTokenRedisEntity> entityOptional =
+                refreshTokenRedisEntityRepository.findByRefreshToken(requestRefreshToken);
+
+        RefreshTokenRedisEntity refreshTokenRedisEntity =
+                entityOptional.orElseThrow(() -> RefreshTokenExpiredException.EXCEPTION);
+
+        Long userId = jwtTokenProvider.parseRefreshToken(requestRefreshToken);
+
+        if (!userId.toString().equals(refreshTokenRedisEntity.getId())) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+
+        User user = userUtils.getUserById(userId);
+
+        String accessToken = jwtTokenProvider.generateAccessToken(userId, user.getAccountRole());
+        String refreshToken = generateRefreshToken(userId);
 
         return AuthTokensResponse.builder()
                 .accessToken(accessToken)
