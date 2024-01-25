f2213ebf2919afcc63bcf6aef42492ef6f81571c
chanwoo <mdsoo55828@gmail.com>
2024-01-25 12:25:00 +0900

    feat : 유저에게 입력받을 정보 요청 dto 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/presentation/dto/request/RegisterRequest.java
@@ -0,0 +1,20 @@
+package uttugseuja.lucklotteryserver.domain.credential.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @Size(max = 10)
+    private String nickname;
+
+    private String email;
+
+    private String profilePath;
+
+}
