b504126e2e3f0dfb996b6313d2ef8f87b33a0aa6
chanwoo <mdsoo55828@gmail.com>
2024-01-26 02:07:38 +0900

    feat : FeignClient를 사용해 인가 코드로 토큰 발급 요청 api 구현

--- src/main/java/uttugseuja/lucklotteryserver/global/api/client/KakaoOauthClient.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/api/client/KakaoOauthClient.java
@@ -1,10 +1,14 @@
 package uttugseuja.lucklotteryserver.global.api.client;
 
 
+import feign.Headers;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
+import uttugseuja.lucklotteryserver.global.api.dto.OauthIdTokenResponse;
 
 @FeignClient(name = "KakaoAuthClient", url = "https://kauth.kakao.com")
 public interface KakaoOauthClient {
@@ -12,4 +16,12 @@ public interface KakaoOauthClient {
     @Cacheable(cacheNames = "KakaoOICD", cacheManager = "oidcKeyCacheManager")
     @GetMapping("/.well-known/jwks.json")
     OIDCKeysResponse getKakaoOIDCOpenKeys();
+
+    @Headers("Content-type: application/x-www-form-urlencoded;charset=utf-8")
+    @PostMapping(
+            "/oauth/token?grant_type=authorization_code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&code={CODE}")
+    OauthIdTokenResponse kakaoAuth(
+            @PathVariable("CLIENT_ID") String clientId,
+            @PathVariable("REDIRECT_URI") String redirectUri,
+            @PathVariable("CODE") String code);
 }
