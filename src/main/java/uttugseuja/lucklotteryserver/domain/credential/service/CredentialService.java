c8f6d32094d919a903bdb8bd70c5844620bf1be0
chanwoo <mdsoo55828@gmail.com>
2024-01-25 11:37:32 +0900

    feat : refreshToken 생성 및 redis 저장 기능

--- src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/CredentialService.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/service/CredentialService.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uttugseuja.lucklotteryserver.domain.credential.domain.RefreshTokenRedisEntity;
+import uttugseuja.lucklotteryserver.domain.credential.domain.repository.RefreshTokenRedisEntityRepository;
 import uttugseuja.lucklotteryserver.domain.credential.presentation.dto.response.CheckRegisteredResponse;
 import uttugseuja.lucklotteryserver.domain.user.domain.User;
 import uttugseuja.lucklotteryserver.domain.user.domain.repository.UserRepository;
+import uttugseuja.lucklotteryserver.global.security.JwtTokenProvider;
 
 
 import java.security.NoSuchAlgorithmException;
@@ -22,6 +25,15 @@ public class CredentialService {
 
     private final UserRepository userRepository;
     private final OauthFactory oauthFactory;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRedisEntityRepository refreshTokenRedisEntityRepository;
+
+    public CheckRegisteredResponse getUserAvailableRegister(String token, OauthProvider oauthProvider) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        OauthStrategy oauthstrategy = oauthFactory.getOauthstrategy(oauthProvider);
+        OIDCDecodePayload oidcDecodePayload = oauthstrategy.getOIDCDecodePayload(token);
+        Boolean isRegistered = !checkUserCanRegister(oidcDecodePayload, oauthProvider);
+        return new CheckRegisteredResponse(isRegistered);
+    }
 
     private Boolean checkUserCanRegister(
             OIDCDecodePayload oidcDecodePayload, OauthProvider oauthProvider) {
@@ -31,13 +43,18 @@ public class CredentialService {
         return user.isEmpty();
     }
 
-    public CheckRegisteredResponse getUserAvailableRegister(String token, OauthProvider oauthProvider) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        OauthStrategy oauthstrategy = oauthFactory.getOauthstrategy(oauthProvider);
-        OIDCDecodePayload oidcDecodePayload = oauthstrategy.getOIDCDecodePayload(token);
-        Boolean isRegistered = !checkUserCanRegister(oidcDecodePayload, oauthProvider);
-        return new CheckRegisteredResponse(isRegistered);
+    private String generateRefreshToken(Long userId) {
+        String refreshToken = jwtTokenProvider.generateRefreshToken(userId);
+        Long tokenExpiredAt = jwtTokenProvider.getRefreshTokenTTlSecond();
+        RefreshTokenRedisEntity build =
+                RefreshTokenRedisEntity.builder()
+                        .id(userId.toString())
+                        .refreshTokenTtl(tokenExpiredAt)
+                        .refreshToken(refreshToken)
+                        .build();
+        refreshTokenRedisEntityRepository.save(build);
+        return refreshToken;
     }
-
 
 }
 
