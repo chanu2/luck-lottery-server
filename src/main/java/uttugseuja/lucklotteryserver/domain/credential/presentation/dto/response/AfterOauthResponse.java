102dbae4dd3a1b37e5512e7cedcc53162aee29f9
chanwoo <mdsoo55828@gmail.com>
2024-01-26 02:04:54 +0900

    feat : 카카오에서 받은 id_token 응답 dto 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/credential/presentation/dto/response/AfterOauthResponse.java
@@ -0,0 +1,11 @@
+package uttugseuja.lucklotteryserver.domain.credential.presentation.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AfterOauthResponse {
+    private String idToken;
+}
