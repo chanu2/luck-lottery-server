55c10d62efd08ffaeff79cce8073a3d1eb716489
ihun-il <hunil9978@gmail.com>
2024-01-27 21:53:59 +0900

    feat : 사용자와 관련된 모든 로또 응답

--- src/main/java/uttugseuja/lucklotteryserver/domain/lottery/presentation/LotteryController.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/lottery/presentation/LotteryController.java
@@ -3,8 +3,11 @@ package uttugseuja.lucklotteryserver.domain.lottery.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.request.CreateLotteryRequest;
+import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.response.LotteryResponse;
 import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.response.RandomLotteryResponse;
 import uttugseuja.lucklotteryserver.domain.lottery.service.LotteryService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +24,10 @@ public class LotteryController {
     @PostMapping("/save")
     public void saveLottery(@RequestBody CreateLotteryRequest createLotteryRequest) {
         lotteryService.saveLottery(createLotteryRequest);
+    }
+
+    @GetMapping("/get")
+    public List<LotteryResponse> getLotteries() {
+        return lotteryService.getLotteriesByUser();
     }
 }
