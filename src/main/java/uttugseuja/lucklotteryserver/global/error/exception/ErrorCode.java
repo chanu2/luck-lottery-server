0d7fd562bb8796d8fc88023cbff5af63614a6b4a
chanwoo <mdsoo55828@gmail.com>
2024-02-02 02:10:23 +0900

    feat :  크롤링 예외처리 에러코드 추가

--- src/main/java/uttugseuja/lucklotteryserver/global/error/exception/ErrorCode.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/error/exception/ErrorCode.java
@@ -18,10 +18,16 @@ public enum ErrorCode {
     /* 404 NOT_FOUND : Resource를 찾을 수 없음 */
     LOTTERY_ROUND_NOT_FOUND(404, "해당 회차의 로또 정보를 찾을 수 없습니다."),
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
-    WINNING_PENSION_LOTTERY_NOT_FOUND(404,  "연금 복권의 당첨 번호를 찾을수 없습니다"),
+    WINNING_PENSION_LOTTERY_가NOT_FOUND(404,  "연금 복권의 당첨 번호를 찾을수 없습니다"),
 
     /* 500 */
-    INTERNAL_SERVER_ERROR(500,"서버 에러")
+    INTERNAL_SERVER_ERROR(500,"서버 에러"),
+
+    /* 500 Internal Server Error: IO 관련 에러 */
+    PAGE_ACCESS_LOTTERY_ERROR(500, "복권 사이트 페이지에 접속할 수 없습니다. URL 또는 네트워크 상태를 확인해 주세요"),
+    CRAWLING_IO_EXCEPTION(500, "크롤링중 입출력 작업 중 에러 발생"),
+    DATA_NOT_FOUND(500, "크롤링으로 가져온 데이터가 Null 입니다"),
+
     ;
 
     private int status;
