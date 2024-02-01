caa52267f94a7706d591e4cfbb72536629e0a955
chanwoo <mdsoo55828@gmail.com>
2024-02-02 00:40:44 +0900

    feat :  ScheduledConfig 설정

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/config/ScheduledConfig.java
@@ -0,0 +1,8 @@
+package uttugseuja.lucklotteryserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class ScheduledConfig {}
