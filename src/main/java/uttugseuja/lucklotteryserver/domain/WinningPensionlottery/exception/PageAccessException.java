86e9270339de1078c459490cf7b5592df9dcc8d0
chanwoo <mdsoo55828@gmail.com>
2024-02-02 02:14:42 +0900

    feat : 크롤링중 네트워크 및 페이지 에러 예외생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/WinningPensionlottery/exception/PageAccessException.java
@@ -0,0 +1,13 @@
+package uttugseuja.lucklotteryserver.domain.WinningPensionlottery.exception;
+
+
+import uttugseuja.lucklotteryserver.global.error.exception.ErrorCode;
+import uttugseuja.lucklotteryserver.global.error.exception.LuckLotteryIoException;
+
+public class PageAccessException extends LuckLotteryIoException {
+    public static final LuckLotteryIoException EXCEPTION = new PageAccessException();
+    private PageAccessException() {
+        super(ErrorCode.PAGE_ACCESS_LOTTERY_ERROR);
+    }
+}
+성
