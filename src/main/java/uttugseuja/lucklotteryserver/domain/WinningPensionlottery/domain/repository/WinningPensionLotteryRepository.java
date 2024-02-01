7fb79dca1a005bed381ad8e8521bbfad18f924c1
chanwoo <mdsoo55828@gmail.com>
2024-02-02 00:35:17 +0900

    feat : 연금 복권 당첨번호 중 가장 최근 회차 데이터 가져오기

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/WinningPensionlottery/domain/repository/WinningPensionLotteryRepository.java
@@ -0,0 +1,9 @@
+package uttugseuja.lucklotteryserver.domain.WinningPensionlottery.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uttugseuja.lucklotteryserver.domain.WinningPensionlottery.domain.WinningPensionLottery;
+import java.util.Optional;
+
+public interface WinningPensionLotteryRepository extends JpaRepository<WinningPensionLottery, Long> {
+    Optional<WinningPensionLottery> findFirstByOrderByRoundDesc();
+}
