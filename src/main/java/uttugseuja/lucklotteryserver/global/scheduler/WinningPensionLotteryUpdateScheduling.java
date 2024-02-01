2977d609802f38c200b91eedd8ec64e0b69ff0c6
chanwoo <mdsoo55828@gmail.com>
2024-02-02 02:28:04 +0900

    feat : 매주 회차 정보를 크롤링으로 가져오는 기능 구현

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/scheduler/WinningPensionLotteryUpdateScheduling.java
@@ -0,0 +1,39 @@
+package uttugseuja.lucklotteryserver.global.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import uttugseuja.lucklotteryserver.domain.WinningPensionlottery.domain.WinningPensionLottery;
+import uttugseuja.lucklotteryserver.domain.WinningPensionlottery.domain.repository.WinningPensionLotteryRepository;
+import uttugseuja.lucklotteryserver.domain.WinningPensionlottery.service.WinningPensionLotteryService;
+import uttugseuja.lucklotteryserver.global.error.exception.LuckLotteryIoException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WinningPensionLotteryUpdateScheduling {
+
+    private final WinningPensionLotteryService winningPensionLotteryService;
+    private final WinningPensionLotteryRepository winningPensionLotteryRepository;
+
+    @Scheduled(cron = "0 0/2 * * * *")
+    //@Scheduled(cron = "0 30/2 19-20 * * THU") 매주 목요일 19시 30분부터 20시 30분까지 2분마다
+    @Transactional
+    public void chatCaching() throws LuckLotteryIoException {
+        log.info("[Scheduling] winning pension lottery update");
+        String round = "";
+        WinningPensionLottery recentWinningPensionLottery = winningPensionLotteryService.getRecentWinningPensionLottery();
+        WinningPensionLottery winningPensionLottery = winningPensionLotteryService.createWinningPensionLottery(round);
+
+        if(recentWinningPensionLottery.getRound() < winningPensionLottery.getRound()){
+            winningPensionLotteryRepository.save(winningPensionLottery);
+        }
+
+
+
+
+
+    }
+}
