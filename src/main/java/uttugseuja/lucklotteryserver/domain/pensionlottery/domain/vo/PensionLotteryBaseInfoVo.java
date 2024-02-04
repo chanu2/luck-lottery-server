4c2e42c58b312601915e7efa607c82fdd1ef8dc5
chanwoo <mdsoo55828@gmail.com>
2024-02-04 20:02:33 +0900

    feat : PensionLottery vo 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/domain/vo/PensionLotteryBaseInfoVo.java
@@ -0,0 +1,25 @@
+package uttugseuja.lucklotteryserver.domain.pensionlottery.domain.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import uttugseuja.lucklotteryserver.global.common.Rank;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class PensionLotteryBaseInfoVo {
+
+    private final Long pensionLotteryId;
+    private final Integer pensionRound;
+    private final Integer pensionGroup;
+    private final Integer pensionFirstNum;
+    private final Integer pensionSecondNum;
+    private final Integer pensionThirdNum;
+    private final Integer pensionFourthNum;
+    private final Integer pensionFifthNum;
+    private final Integer pensionSixthNum;
+    private final Rank rank;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifyDate;
+}
+
