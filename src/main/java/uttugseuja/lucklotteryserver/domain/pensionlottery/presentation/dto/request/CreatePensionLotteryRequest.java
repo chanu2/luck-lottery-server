c246bd224a1d9da55a5cb96446bccba0e4a4ba1b
chanwoo <mdsoo55828@gmail.com>
2024-02-04 13:48:03 +0900

    feat : 연금 복권 번호를 입력 받는 dto 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/presentation/dto/request/CreatePensionLotteryRequest.java
@@ -0,0 +1,20 @@
+package uttugseuja.lucklotteryserver.domain.pensionlottery.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class CreatePensionLotteryRequest {
+
+    private Integer pensionRound;
+    private Integer pensionGroup;
+    private Integer pensionFirstNum;
+    private Integer pensionSecondNum;
+    private Integer pensionThirdNum;
+    private Integer pensionFourthNum;
+    private Integer pensionFifthNum;
+    private Integer pensionSixthNum;
+
+}
