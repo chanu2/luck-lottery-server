0d9234afd9cee619ebe4a712e3aa590b8c4130be
ihun-il <hunil9978@gmail.com>
2024-02-06 22:03:16 +0900

    feat : 1~45 숫자만 허용, null값은 허용하지 않는다.

--- src/main/java/uttugseuja/lucklotteryserver/domain/lottery/presentation/dto/request/CreateLotteryRequest.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/lottery/presentation/dto/request/CreateLotteryRequest.java
@@ -1,5 +1,8 @@
 package uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,15 +10,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateLotteryRequest {
 
+    @NotNull(message = "null값을 허용하지 않습니다.")
+    @Min(value = 1, message = "1 미만의 값을 허용하지 않습니다.")
+    @Max(value = 45, message = "45 초과의 값을 허용하지 않습니다.")
     private Integer firstNum;
 
+    @NotNull(message = "null값을 허용하지 않습니다.")
+    @Min(value = 1, message = "1 미만의 값을 허용하지 않습니다.")
+    @Max(value = 45, message = "45 초과의 값을 허용하지 않습니다.")
     private Integer secondNum;
 
+    @NotNull(message = "null값을 허용하지 않습니다.")
+    @Min(value = 1, message = "1 미만의 값을 허용하지 않습니다.")
+    @Max(value = 45, message = "45 초과의 값을 허용하지 않습니다.")
     private Integer thirdNum;
 
+    @NotNull(message = "null값을 허용하지 않습니다.")
+    @Min(value = 1, message = "1 미만의 값을 허용하지 않습니다.")
+    @Max(value = 45, message = "45 초과의 값을 허용하지 않습니다.")
     private Integer fourthNum;
 
+    @NotNull(message = "null값을 허용하지 않습니다.")
+    @Min(value = 1, message = "1 미만의 값을 허용하지 않습니다.")
+    @Max(value = 45, message = "45 초과의 값을 허용하지 않습니다.")
     private Integer fifthNum;
 
+    @NotNull(message = "null값을 허용하지 않습니다.")
+    @Min(value = 1, message = "1 미만의 값을 허용하지 않습니다.")
+    @Max(value = 45, message = "45 초과의 값을 허용하지 않습니다.")
     private Integer sixthNum;
 }
