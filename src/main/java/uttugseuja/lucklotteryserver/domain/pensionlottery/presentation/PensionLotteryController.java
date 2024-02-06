88e21c70e584a67ad905594789d5201704ef3bd5
chanwoo <mdsoo55828@gmail.com>
2024-02-06 14:20:56 +0900

    feat : 나의 연급복권 상태를 조회하는 api 구현

--- src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/presentation/PensionLotteryController.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/presentation/PensionLotteryController.java
@@ -3,9 +3,10 @@ package uttugseuja.lucklotteryserver.domain.pensionlottery.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import uttugseuja.lucklotteryserver.domain.pensionlottery.presentation.dto.request.CreatePensionLotteryRequest;
+import uttugseuja.lucklotteryserver.domain.pensionlottery.presentation.dto.response.PensionLotteryResponse;
 import uttugseuja.lucklotteryserver.domain.pensionlottery.presentation.dto.response.RandomPensionLotteryResponse;
 import uttugseuja.lucklotteryserver.domain.pensionlottery.service.PensionLotteryService;
-
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +23,11 @@ public class PensionLotteryController {
     @PostMapping("/save")
     public void savePensionLottery(@RequestBody CreatePensionLotteryRequest createPensionLotteryRequest) {
         pensionLotteryService.savePensionLottery(createPensionLotteryRequest);
+    }
+
+    @GetMapping("/get")
+    public List<PensionLotteryResponse> getLotteries() {
+        return pensionLotteryService.getPensionLottery();
     }
 
 }
