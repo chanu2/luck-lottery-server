4a908dd8c3425010a8d5d10ddc17651a1d516915
ihun-il <hunil9978@gmail.com>
2024-02-06 17:33:48 +0900

    feat : 회차와 관련된 당첨 로또 DB에서 조회

--- src/main/java/uttugseuja/lucklotteryserver/domain/winning_lottery/domain/repository/WinningLotteryRepository.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/winning_lottery/domain/repository/WinningLotteryRepository.java
@@ -3,5 +3,8 @@ package uttugseuja.lucklotteryserver.domain.winning_lottery.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uttugseuja.lucklotteryserver.domain.winning_lottery.domain.WinningLottery;
 
+import java.util.Optional;
+
 public interface WinningLotteryRepository extends JpaRepository<WinningLottery, Long> {
+    Optional<WinningLottery> findByRound(Integer round);
 }
