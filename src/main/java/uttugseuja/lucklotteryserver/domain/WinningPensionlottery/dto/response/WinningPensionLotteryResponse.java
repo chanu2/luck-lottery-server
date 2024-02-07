6751d7f344bb1971dfb205d0a2356bf7ab2b8c03
chanwoo <mdsoo55828@gmail.com>
2024-02-07 23:53:41 +0900

    feat : 당첨 연금 복권 정보 dto 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/WinningPensionlottery/dto/response/WinningPensionLotteryResponse.java
@@ -0,0 +1,44 @@
+package uttugseuja.lucklotteryserver.domain.WinningPensionlottery.dto.response;
+
+import lombok.Getter;
+import uttugseuja.lucklotteryserver.domain.WinningPensionlottery.domain.vo.WinningPensionLotteryBaseInfoVo;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class WinningPensionLotteryResponse {
+
+    private Integer round;
+    private LocalDateTime winningDate;
+    private Integer lotteryGroup;
+    private Integer winningFirstNum;
+    private Integer winningSecondNum;
+    private Integer winningThirdNum;
+    private Integer winningFourthNum;
+    private Integer winningFifthNum;
+    private Integer winningSixthNum;
+    private Integer bonusFirstNum;
+    private Integer bonusSecondNum;
+    private Integer bonusThirdNum;
+    private Integer bonusFourthNum;
+    private Integer bonusFifthNum;
+    private Integer bonusSixthNum;
+
+    public WinningPensionLotteryResponse(WinningPensionLotteryBaseInfoVo winningPensionLotteryBaseInfoVo) {
+        round = winningPensionLotteryBaseInfoVo.getRound();
+        winningDate = winningPensionLotteryBaseInfoVo.getLotteryDrawTime();
+        lotteryGroup = winningPensionLotteryBaseInfoVo.getLotteryGroup();
+        winningFirstNum = winningPensionLotteryBaseInfoVo.getWinningFirstNum();
+        winningSecondNum = winningPensionLotteryBaseInfoVo.getWinningSecondNum();
+        winningThirdNum = winningPensionLotteryBaseInfoVo.getWinningThirdNum();
+        winningFourthNum = winningPensionLotteryBaseInfoVo.getWinningFourthNum();
+        winningFifthNum = winningPensionLotteryBaseInfoVo.getWinningFifthNum();
+        winningSixthNum = winningPensionLotteryBaseInfoVo.getWinningSixthNum();
+        bonusFirstNum = winningPensionLotteryBaseInfoVo.getBonusFirstNum();
+        bonusSecondNum = winningPensionLotteryBaseInfoVo.getBonusFirstNum();
+        bonusThirdNum = winningPensionLotteryBaseInfoVo.getBonusFirstNum();
+        bonusFourthNum = winningPensionLotteryBaseInfoVo.getBonusFirstNum();
+        bonusFifthNum = winningPensionLotteryBaseInfoVo.getBonusFirstNum();
+        bonusSixthNum = winningPensionLotteryBaseInfoVo.getBonusFirstNum();
+    }
+}
