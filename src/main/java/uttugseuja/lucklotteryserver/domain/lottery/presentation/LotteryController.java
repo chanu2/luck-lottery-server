d8b54dc49d602f62da91d927c8e6834ced9be222
ihun-il <hunil9978@gmail.com>
2024-02-08 16:49:03 +0900

    feat : Controller 변경

--- src/main/java/uttugseuja/lucklotteryserver/domain/lottery/presentation/LotteryController.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/lottery/presentation/LotteryController.java
@@ -1,16 +1,18 @@
 package uttugseuja.lucklotteryserver.domain.lottery.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.*;
 import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.request.CreateLotteryRequest;
-import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.response.LotteryResponse;
+import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.response.OneRoundResponse;
 import uttugseuja.lucklotteryserver.domain.lottery.presentation.dto.response.RandomLotteryResponse;
 import uttugseuja.lucklotteryserver.domain.lottery.service.LotteryService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ public class LotteryController {
     private final LotteryService lotteryService;
 
     @GetMapping("/random")
+    @Operation(summary = "랜덤 로또 번호 생성 API")
     public RandomLotteryResponse createRandomLottery() {
         return lotteryService.createRandomLottery();
     }
@@ -32,7 +35,16 @@ public class LotteryController {
     }
 
     @GetMapping("/get")
-    public List<LotteryResponse> getLotteries() {
-        return lotteryService.getLotteriesByUser();
+    @Operation(summary = "회차별 사용자 로또 목록 조회 API")
+    @Parameters({
+            @Parameter(name = "page", description = "Page number", example = "0", required = false),
+            @Parameter(name = "size", description = "Page size", example = "10", required = false)
+    })
+    public Slice<OneRoundResponse> getLotteries(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                @RequestParam(value = "size", defaultValue = "10") Integer size) {
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        return lotteryService.getLotteriesByUser(pageRequest);
     }
 }
