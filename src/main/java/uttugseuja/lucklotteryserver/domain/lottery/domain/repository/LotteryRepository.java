efcafa77a4787c05ad8dedd8aacab650751dd809
ihun-il <hunil9978@gmail.com>
2024-02-08 16:47:41 +0900

    feat : 1. round 페이징으로 변경 2. round가 recentround보다 작거나 같은 로또 중에서 rank가 null값인 Lottery 조회

--- src/main/java/uttugseuja/lucklotteryserver/domain/lottery/domain/repository/LotteryRepository.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/lottery/domain/repository/LotteryRepository.java
@@ -1,11 +1,21 @@
 package uttugseuja.lucklotteryserver.domain.lottery.domain.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uttugseuja.lucklotteryserver.domain.lottery.domain.Lottery;
 import uttugseuja.lucklotteryserver.domain.user.domain.User;
 
 import java.util.List;
 
+
 public interface LotteryRepository extends JpaRepository<Lottery, Long> {
+    @Query("select distinct l.round from Lottery l where l.user = :user order by l.round desc")
+    Slice<Integer> findRoundByUser(User user, Pageable pageable);
+
+    @Query("SELECT l FROM Lottery l WHERE l.user = :user AND l.rank IS NULL AND l.round <= :round")
+    List<Lottery> findLotteriesByUserAndRound(User user, Integer round);
+
     List<Lottery> findByUser(User user);
 }
