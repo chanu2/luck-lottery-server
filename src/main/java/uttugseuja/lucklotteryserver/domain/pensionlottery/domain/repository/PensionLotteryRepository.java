d640d758b73e6672d328fecf2fc787061334102f
chanwoo <mdsoo55828@gmail.com>
2024-02-10 00:21:16 +0900

    feat : 최근 회차 이하 연금복권을 가져오는 쿼리 작성

--- src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/domain/repository/PensionLotteryRepository.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/domain/repository/PensionLotteryRepository.java
@@ -3,14 +3,17 @@ package uttugseuja.lucklotteryserver.domain.pensionlottery.domain.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import uttugseuja.lucklotteryserver.domain.lottery.domain.Lottery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uttugseuja.lucklotteryserver.domain.pensionlottery.domain.PensionLottery;
 import uttugseuja.lucklotteryserver.domain.user.domain.User;
-
 import java.util.List;
 
 public interface PensionLotteryRepository extends JpaRepository<PensionLottery, Long> {
-    //List<PensionLottery> findByUserId(Long id);
 
     Slice<PensionLottery> findByUserId(Long userId, Pageable pageable);
+
+    @Query("select p from PensionLottery p where p.user = :user and p.rank is null and p.pensionRound <= :pensionRound")
+    List<PensionLottery> drawnPensionLottery(User user, Integer pensionRound);
+
 }
