0715cd7d8700dca88d3893ecc9f1e1aede84c635
chanwoo <mdsoo55828@gmail.com>
2024-02-11 14:06:15 +0900

    feat : 램덤 프로필 이미지 가져오기 쿼리 작성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/asset/domain/repository/ProfileImageRepository.java
@@ -0,0 +1,13 @@
+package uttugseuja.lucklotteryserver.domain.asset.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import uttugseuja.lucklotteryserver.domain.asset.domain.ProfileImage;
+import java.util.Optional;
+
+public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
+
+    @Query(value = "SELECT * FROM tbl_profile_image order by RAND() limit 1", nativeQuery = true)
+    Optional<ProfileImage> findRandomProfileImage();
+
+}
