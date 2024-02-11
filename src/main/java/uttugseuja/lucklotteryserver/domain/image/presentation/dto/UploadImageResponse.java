f7eda3009f1e71800dd58019f24a6b2541bca98b
chanwoo <mdsoo55828@gmail.com>
2024-02-11 13:46:43 +0900

    feat : 이미지 업로드 응답 dto 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/domain/image/presentation/dto/UploadImageResponse.java
@@ -0,0 +1,11 @@
+package uttugseuja.lucklotteryserver.domain.image.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UploadImageResponse {
+
+    private final String imageUrl;
+}
