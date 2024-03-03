7811d5be2c9e146de78c8378352d11251cb80f90
ihun-il <hunil9978@gmail.com>
2024-03-03 20:16:59 +0900

    feat : Event 발행하는 Event Publisher 생성

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/event/Events.java
@@ -0,0 +1,25 @@
+package uttugseuja.lucklotteryserver.global.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+
+@Slf4j
+public class Events {
+    private static ThreadLocal<ApplicationEventPublisher> publisherLocal = new ThreadLocal<>();
+
+    public static void raise(DomainEvent event) {
+        if (event == null) return;
+
+        if (publisherLocal.get() != null) {
+            publisherLocal.get().publishEvent(event);
+        }
+    }
+
+    static void setPublisher(ApplicationEventPublisher publisher) {
+        publisherLocal.set(publisher);
+    }
+
+    static void reset() {
+        publisherLocal.remove();
+    }
+}
