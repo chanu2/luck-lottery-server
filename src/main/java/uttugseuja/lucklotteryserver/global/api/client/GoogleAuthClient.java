002a1e05dd75de23181a6ed11bbe3061b88720b3
chanwoo <mdsoo55828@gmail.com>
2024-03-04 21:51:43 +0900

    feat:  FeignClient 사용하여 Google Oauth accessToken 에서 유저 정보 가져오는 기능 구현

--- src/main/java/uttugseuja/lucklotteryserver/global/api/client/GoogleAuthClient.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/api/client/GoogleAuthClient.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import uttugseuja.lucklotteryserver.global.api.dto.UserInfoToOauthDto;
 import uttugseuja.lucklotteryserver.global.api.dto.OIDCKeysResponse;
-import uttugseuja.lucklotteryserver.global.api.dto.OauthIdTokenResponse;
+import uttugseuja.lucklotteryserver.global.api.dto.OauthTokenResponse;
 
 @FeignClient(name = "GoogleAuthClient", url = "https://www.googleapis.com/oauth2")
 public interface GoogleAuthClient {
@@ -15,7 +17,7 @@ public interface GoogleAuthClient {
     @Headers("Content-type: application/x-www-form-urlencoded;charset=utf-8")
     @PostMapping(
             "/v4/token?grant_type=authorization_code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&code={CODE}&client_secret={CLIENT_SECRET}")
-    OauthIdTokenResponse googleAuth(
+    OauthTokenResponse googleAuth(
             @PathVariable("CLIENT_ID") String clientId,
             @PathVariable("REDIRECT_URI") String redirectUri,
             @PathVariable("CODE") String code,
@@ -24,5 +26,9 @@ public interface GoogleAuthClient {
     @Cacheable(cacheNames = "GoogleOICD", cacheManager = "oidcKeyCacheManager")
     @GetMapping("/v3/certs")
     OIDCKeysResponse getGoogleOIDCOpenKeys();
+
+    @GetMapping("/v1/userinfo?alt=json")
+    UserInfoToOauthDto getGoogleInfo(@RequestHeader("Authorization") String token);
+
 }
 
