efd798a527aa6b718874864ee28b862b4a26338b
chanwoo <mdsoo55828@gmail.com>
2024-03-04 21:57:27 +0900

    feat:  Oauth accessToken 에서 유저 정보 가져오는 Dto 구현

---
+++ src/main/java/uttugseuja/lucklotteryserver/global/api/dto/UserInfoToOauthDto.java
@@ -0,0 +1,11 @@
+package uttugseuja.lucklotteryserver.global.api.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserInfoToOauthDto {
+    private String email;
+    private String id;
+}
