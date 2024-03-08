46b2ed0e1a199c7a3952ec19dd186a0279cbb615
chanwoo <mdsoo55828@gmail.com>
2024-03-08 19:25:34 +0900

    fix: 로그인 관련 시큐리티 적용 및 admin 적용

--- src/main/java/uttugseuja/lucklotteryserver/global/config/SecurityConfig.java
+++ src/main/java/uttugseuja/lucklotteryserver/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uttugseuja.lucklotteryserver.domain.user.domain.AccountRole;
 import uttugseuja.lucklotteryserver.global.error.exception.ExceptionFilter;
 import uttugseuja.lucklotteryserver.global.security.JwtTokenFilter;
 import uttugseuja.lucklotteryserver.global.security.JwtTokenProvider;
@@ -36,15 +38,25 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests((registry) ->
-                        registry.requestMatchers("/api/v1/credentials/**").permitAll()
-                                .requestMatchers("/api/signup").permitAll()
+                        registry.requestMatchers("/api/v1/credentials/login2/**","/api/v1/credentials/singup2",
+                                        "/api/v1/credentials/oauth/link/kakao","/api/v1/credentials/oauth/kakao","/api/v1/credentials/oauth/link/google"
+                                        ,"/api/v1/credentials/oauth/google","/api/v1/credentials/oauth/valid/register","/api/v1/credentials/login"
+                                        ,"/api/v1/credentials/register","/api/v1/credentials/refresh").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/api/v1/winning/pension/lottery/admin/save/db").hasRole(AccountRole.ADMIN.getValue())
                                 .anyRequest().authenticated()
                 );
 
 
         return http.build();
+    }
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
     }
 
 }
