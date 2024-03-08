f5528bc83145086ff6f18c230efe70e67ed38442
chanwoo <mdsoo55828@gmail.com>
2024-03-08 19:20:50 +0900

    fix: 연금 복권 저장시 valid min,max 적용

--- src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/presentation/dto/request/CreatePensionLotteryRequest.java
+++ src/main/java/uttugseuja/lucklotteryserver/domain/pensionlottery/presentation/dto/request/CreatePensionLotteryRequest.java
@@ -1,19 +1,33 @@
 package uttugseuja.lucklotteryserver.domain.pensionlottery.presentation.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @Getter
 @NoArgsConstructor
 public class CreatePensionLotteryRequest {
 
+    @Min(1) @Max(5)
     private Integer pensionGroup;
+
+    @Min(0) @Max(9)
     private Integer pensionFirstNum;
+
+    @Min(0) @Max(9)
     private Integer pensionSecondNum;
+
+    @Min(0) @Max(9)
     private Integer pensionThirdNum;
+
+    @Min(0) @Max(9)
     private Integer pensionFourthNum;
+
+    @Min(0) @Max(9)
     private Integer pensionFifthNum;
+
+    @Min(0) @Max(9)
     private Integer pensionSixthNum;
 
 }
