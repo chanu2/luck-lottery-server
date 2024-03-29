fc1b0916c0a8840e8ae3fae2fe247928a29e1043
chanwoo <mdsoo55828@gmail.com>
2024-03-29 11:53:18 +0900

    feat: aws 서버 시간 서울로 설정

--- src/main/java/uttugseuja/lucklotteryserver/LucklotteryserverApplication.java
+++ src/main/java/uttugseuja/lucklotteryserver/LucklotteryserverApplication.java
@@ -1,12 +1,20 @@
 package uttugseuja.lucklotteryserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class LucklotteryserverApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LucklotteryserverApplication.class, args);
